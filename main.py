24d09da928e77e28c3e1f07be98ea59bc85a12e4
Aktansensei <whoisaktan@gmail.com>
2026-05-12 13:13:29 +0600

    fix: serve index.html on root route GET /

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 from database import Base, engine
@@ -22,6 +23,11 @@ app.include_router(message_router)
 app.include_router(websocket_router)
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+
+@app.get("/", include_in_schema=False)
+async def serve_frontend():
+    return FileResponse("static/index.html")
 
 
 @app.get("/health")
